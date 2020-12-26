454d31644af32a9c6d2369b7650761cf17d9d25f
Dundaki <vigjoaopaulo@gmail.com>
2020-12-26 10:49:59 -0300

    versao0

--- src/gitHubTeste/Program.java
+++ src/gitHubTeste/Program.java
@@ -1,9 +1,33 @@
 package gitHubTeste;
 
-public class Program {
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
 
-	public static void main(String[] args) {
-		System.out.println("Ola mundo!!");
+public class Program {
+	public static void main(String[] args) throws ParseException{
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		System.out.print("Room number: ");
+		int n =  sc.nextInt();
+		
+		System.out.print("Chek-in data (dd/MM/yyyy)");
+		Date checkIn = sdf.parse(sc.next()); 
+		
+		System.out.print("Chek-in data (dd/MM/yyyy)");
+		Date checkOut = sdf.parse(sc.next()); 
+		
+		if(!checkOut.after(checkIn)) {
+			System.out.println("Error in reservation: Check-out data"
+					+ "must be after check-in");
+		}else {
+			System.out.println("teste");
+		}
+		
+		
+		sc.close();
 	}
 
 }
